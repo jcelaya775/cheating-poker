cf3ce992cc7e5bef35eea221164108ba26e7fb26
Jorge Celaya <jcelaya775@gmail.com>
2022-09-24 09:57:32 -0500

    first commit

--- poker.py
+++ poker.py
@@ -1,12 +1,12 @@
-from random import randrange
+from random import randint
 
 # Create a deck of cards format: [[suit, value], [suit, value], ...]
 temp = [[suit, value] for suit in ["Clubs", "Hearts", "Spades", "Diamonds"] for value in range(1, 14)]
 
-# shuffle deck
-deck = temp
+# # shuffle deck
+deck = []
 while temp:
-    randIdx = randrange(1, len(deck))
+    randIdx = randint(0, len(temp) - 1)
     deck.append(temp[randIdx])
+    del temp[randIdx]
 
-print(deck)
